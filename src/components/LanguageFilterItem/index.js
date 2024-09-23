40d25d6d36e49f818fd3d53f3030edf4d76c9eb6
Surytejaswini66 <suryaviswanadhapalli666@gmail.com>
2024-09-23 09:22:01 +0000

    solution

---
+++ src/components/LanguageFilterItem/index.js
@@ -0,0 +1,29 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {languageFilterItemDetails, isActive, setActiveLanguageBtn} = props
+  const {id, language} = languageFilterItemDetails
+  const mainBtnClass = isActive
+    ? 'language-btn active-language-btn'
+    : 'language-btn'
+
+  const onClickLanguageBtn = () => {
+    setActiveLanguageBtn(id)
+  }
+
+  return (
+    <li>
+      <button
+        className={mainBtnClass}
+        onClick={onClickLanguageBtn}
+        type="button"
+      >
+        {language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
